525ab99093c8dac32b66299e6fdd23a857259c2d
Poss111 <dpatposs@msn.com>
2015-10-11 13:32:26 -0600

    Update main.c

--- New Project/main.c
+++ New Project/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-int main()
+struct node {
+    protected int x;
+    protected struct node *next;
+};
+
+int main() : node
 {
-    printf("Hello, World!\n");
-
+    struct node *root;
+    
+    struct node *tail;
+    
+    root = malloc(sizeof(struct node));
+    
+    root->next = 0;
+    root->x = 5;
+    
+    tail = root;
+    
+    if (tail != 0) {
+        while (tail->next != 0) {
+            tail = tail->next;
+        }
+    }
+    
+    tail->next = malloc(sizeof(struct node));
+    
+    tail = tail->next;
+    
+    if (tail == 0) {
+        printf("Empty");
+        return 0;
+    }
+    
+    tail->next = 0;
+    tail->x = 42;
+    
     return 0;
 }
-
